5a9367069cd42d10cded9623bdba0a61fb38e40d
palmparames1996 <parames.koenson@gmail.com>
2024-01-17 16:21:24 +0700

    additional model

--- resume-api/app/main.py
+++ resume-api/app/main.py
@@ -16,9 +16,9 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/resume_matching")
-def inference_resume_match(jd_path: str, cv_path: str):
+def inference_resume_match(jd_path: str, cv_path: str, model_id: int = 1):
     list_jd_path = [os.path.join(VOLUMN_PATH,i) for i in jd_path.split(',')]
     list_cv_path = [os.path.join(VOLUMN_PATH,i) for i in cv_path.split(',')]
-    matching_result = inferencing_matching(list_jd_path, list_cv_path)
+    matching_result = inferencing_matching(list_jd_path, list_cv_path, model_id)
     extract_result = inferencing_extract(list_cv_path)
     return {'matching_result' : matching_result, 'extract_result' : extract_result}
--- resume-api/app/resume.py
+++ resume-api/app/resume.py
@@ -2,6 +2,9 @@ import PyPDF2, pdfplumber
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from pyresparser import ResumeParser
+import difflib
+import jellyfish
+from fuzzywuzzy import fuzz
 
 def pdfToText(path):
     open_session = open(path,'rb')
@@ -18,12 +21,26 @@ def pdfToText(path):
     text_respone = text_respone.replace("\n"," ")
     return text_respone
 
-def MatchPercentageText(text1,text2,round_digit=2): 
-    feature_model = CountVectorizer()
-    count_matrix = feature_model.fit_transform([text1, text2])
-    MatchPercentage = cosine_similarity(count_matrix)[0][1]*100
-    MatchPercentage = round(MatchPercentage,round_digit)
-    return MatchPercentage
+def MatchPercentageText(text1,text2,model_id,round_digit=2): 
+    if model_id == 1:
+        feature_model = CountVectorizer()
+        count_matrix = feature_model.fit_transform([text1, text2])
+        MatchPercentage = cosine_similarity(count_matrix)[0][1]*100
+        MatchPercentage = round(MatchPercentage,round_digit)
+    elif model_id == 2:
+        MatchPercentage = difflib.SequenceMatcher(None, text1, text2).ratio()*100
+        MatchPercentage = round(MatchPercentage,round_digit)
+    elif model_id == 3:
+        MatchPercentage = jellyfish.levenshtein_distance(text1, text2) ## 3000+
+    elif model_id == 4:
+        MatchPercentage = fuzz.ratio(text1, text2) 
+    elif model_id == 5:
+        MatchPercentage = fuzz.partial_ratio(text1, text2) # same 4
+    elif model_id == 6:
+        MatchPercentage = fuzz.token_sort_ratio(text1, text2)
+    elif model_id == 7:
+        MatchPercentage = fuzz.token_set_ratio(text1, text2)
+    return round(MatchPercentage,round_digit)
 
 def preprocessingToText(path):
     if '.pdf' in path:
@@ -40,13 +57,13 @@ def extract_job_attributes(file_path):
     data = session_resume.get_extracted_data()
     return data
 
-def inferencing_matching(list_jd_path: list, list_cv_path: list):
+def inferencing_matching(list_jd_path: list, list_cv_path: list, model_id: int):
     list_respone = []
     for jd_path in list_jd_path :
         jd_text = preprocessingToText(jd_path)
         for cv_path in list_cv_path :
             cv_text = preprocessingToText(cv_path)
-            matching_percent = MatchPercentageText(jd_text, cv_text)
+            matching_percent = MatchPercentageText(jd_text, cv_text, model_id)
             list_respone.append({'jd_path' : jd_path, 'cv_path' : cv_path, 'matching_percent' : matching_percent})
     return list_respone
 
